07c845781bbbd85731ef74eb4acf30a6303d8ae7
Thanus Kumaar <thanuskumaara@gmail.com>
2025-04-02 16:07:27 +0530

    created basic file structure

--- frontend.py
+++ frontend.py
@@ -1,4 +1,11 @@
 import streamlit as st
 from main import *
-st.title("Raga Classifier")
-st.subheader("Find out what raga a song is set in!")
+
+homePage = st.Page("./pages/home.py", title="Home", url_path="/")
+modelPage = st.Page("./pages/model_metrics.py", title="Model Metrics", url_path="/model")
+uploadPage = st.Page("./pages/upload.py", title="Uploads", url_path="/upload")
+
+pg = st.navigation([homePage, modelPage, uploadPage])
+
+
+pg.run()
